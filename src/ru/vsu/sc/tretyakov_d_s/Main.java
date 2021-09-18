fef99a03bbe3e690e1f792b2847c0d9d4eb9bd29
Ertnad <mrgerchannel@gmail.com>
2021-09-18 13:13:31 +0300

    v1.1

--- src/ru/vsu/sc/tretyakov_d_s/Main.java
+++ src/ru/vsu/sc/tretyakov_d_s/Main.java
@@ -6,30 +6,59 @@ public class Main {
 
   public static void main(String[] args) {
 
-    System.out.print("Введите собственную скорость лодки: V = ");
-    int V = readNumber();
-    System.out.print("Введите скорость реки: U = ");
-    int U = readNumber();
+    double V = readSpeed("Boat");
+    double U = readSpeed("River");
+    checkSpeed(V,U);
+    double T1 = readTime("Driving along the river");
+    double T2 = readTime("Driving against the flow of the river");
 
-    if (U >= V) {
-      do {
-        System.out.println("Скорость течения реки должна быть меньше собственной скорости лодки");
-        System.out.print("Введите скорость реки: U = ");
-        U = readNumber();
-      } while (U >= V);
-    }
-
-    System.out.print("Введите время движения лодки по озеру: T1 = ");
-    int T1 = readNumber();
-    System.out.print("Введите время движения лодки по реке: T2 = ");
-    int T2 = readNumber();
-    int S = (T1 * V) + (T2 * (V - U));
-
-    System.out.printf("Путь, пройденный лодкой S = %d", S);
+    double S = findSum(V,U,T1,T2);
+    printResult(S);
   }
 
-  public static int readNumber() {
-    Scanner scanner = new Scanner(System.in);
-    return scanner.nextInt();
+  private static double readSpeed(String speedType) {
+    System.out.printf("Enter %s speed - ", speedType);
+
+    Scanner scn = new Scanner(System.in);
+    double value = scn.nextDouble();
+
+    if(value < 0) {
+      System.out.println("Speed can be only more than 0");
+      System.exit(1);
+    }
+
+
+
+    return value;
+  }
+
+  private static void checkSpeed(double V, double U) {
+    if (U >= V) {
+      do {
+        System.out.println("The speed of the river must be less than the boat's own speed");
+        U = readSpeed("River");
+      } while (U >= V);
+    }
+  }
+
+  private static double readTime(String timeType) {
+    System.out.printf("Enter %s time - ", timeType);
+
+    Scanner scn = new Scanner(System.in);
+    double value = scn.nextDouble();
+
+    if(value < 0) {
+      System.out.println("Time can be only more than 0");
+      System.exit(1);
+    }
+    return value;
+  }
+
+  private static double findSum(double V, double U, double T1, double T2) {
+    return (V*T1 + (V-U)*T2);
+  }
+
+  public static void printResult(double S) {
+    System.out.printf("The path traveled by the boat S = %s", S);
   }
 }
